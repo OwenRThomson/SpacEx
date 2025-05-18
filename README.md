bc2fdef201d5d53c10b2e99c08c869ff6523d48a
Wildboar14 <92429185+OwenRThomson@users.noreply.github.com>
2025-05-18 19:29:20 +0100

    feat: :sparkles: Display dashboard w/ Navbar

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. I then defined the types based on CURL commands and the design doc I was given
 3. Setup server, I created functions to call the API given and called it from sveltekit server.
 4. Create svelte store for dashboard, in charge of querying the API and showing results
+5. Create components for charts, use in dashboard component. Get the data to display in this from (4)
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { NavBrand, Navbar } from 'flowbite-svelte';
+
+	import Icon from '@iconify/svelte';
+	import Filters from './Filters.svelte';
+</script>
+
+<Navbar class="mx-10 sm:mx-0">
+	<NavBrand href="/" class="mx-10">
+		<div class="flex flex-row items-center gap-x-4">
+			<Icon icon="mdi:space-station" width="24" height="24" style="color: #008cfb" />
+			<span
+				class="ml-px self-center text-xl font-semibold whitespace-nowrap sm:text-2xl dark:text-white"
+			>
+				SpacEx*
+			</span>
+
+			<span class="text-sm font-normal text-gray-500 dark:text-gray-400"
+				>The legally distinct space trading tool</span
+			>
+		</div>
+	</NavBrand>
+	<div class="hidden lg:block lg:ps-3">
+		<Filters />
+	</div>
+</Navbar>
