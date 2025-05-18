4cd4faba4c358324eebdf0c21567fe8681999d67
Wildboar14 <92429185+OwenRThomson@users.noreply.github.com>
2025-05-18 19:02:51 +0100

    feat: :label: Define types

---
+++ src/lib/types.ts
@@ -0,0 +1,62 @@
+// I started off by defining the types. Makes it easier for me to think about what I'm working with.
+
+// This involved curling the commands initially to see what the API returned, and then defining the types based on that.
+
+export type JellyfaasToken = {
+	token: string;
+	expiry: string;
+};
+
+export type SpaceStation =
+	| 'Alpha Centauri'
+	| 'Beta Hydri'
+	| 'Gamma Draconis'
+	| 'Delta Pavonis'
+	| 'Epsilon Indi'
+	| 'all-stations';
+
+export type Artifact =
+	| 'Gold'
+	| 'Silver'
+	| 'Food Rations'
+	| 'Water'
+	| 'Medicine'
+	| 'Steel'
+	| 'Platinum'
+	| 'Computer Chips'
+	| 'Fuel Cells'
+	| 'Alien Artifacts'
+	| 'Spice'
+	| 'Rare Gems'
+	| 'all-artifacts';
+
+export type JellyfaasApiQuery = {
+	station?: SpaceStation;
+	artifact?: Artifact;
+	historic?: boolean;
+};
+
+export type ArtifactWithPrice = {
+	name: Artifact;
+	history: number[];
+	price: number;
+};
+
+export type StationWithArtifacts = {
+	name: SpaceStation;
+	artifacts: ArtifactWithPrice[];
+};
+
+export type ArtifactWithHistory = {
+	name: Artifact;
+	prices: {
+		// This is what I wanted the data structure to be as it would be easier to work with rather than price & history
+		date: string;
+		price: number;
+	}[];
+};
+
+export type StationWithArtifactsHistory = {
+	name: SpaceStation;
+	artifacts: ArtifactWithHistory[];
+};
